daf56bbb8fc21aa949077a63b78be089d1d11f62
glebanazarenko <glebanazarenko@gmail.com>
2025-04-01 04:01:48 +0300

    Добавил поиск дома и страницу для дома

--- services/backend/src/crud/houses.py
+++ services/backend/src/crud/houses.py
@@ -7,12 +7,12 @@ from tortoise.expressions import Q
 from tortoise.queryset import QuerySet
 from src.database.models import House
 from src.schemas.houses import HouseOutSchema
+from uuid import UUID
 
 
 
 
 async def get_house(query: str, page: int = 1, per_page: int = 10) -> List[HouseOutSchema]:
-    logger.info(f'Начало поиска дома {query}')
     offset = (page - 1) * per_page
     houses = await House.filter(
         Q(unom__icontains=query) |
@@ -20,9 +20,20 @@ async def get_house(query: str, page: int = 1, per_page: int = 10) -> List[House
         Q(simple_address__icontains=query)
     ).offset(offset).limit(per_page).all()
 
-    logger.info(f'House data: {houses}')
-
     if houses:
         # Используем HouseOutSchema для сериализации данных
         return [await HouseOutSchema.from_tortoise_orm(house) for house in houses]
-    raise HTTPException(status_code=400, detail="Нет такого дома")
+    raise HTTPException(status_code=404, detail="Нет такого дома")
+
+
+async def get_house_by_id(id: UUID) -> HouseOutSchema:
+    house = await House.filter(Q(id=id)).first()
+
+    if house:
+        # Используем HouseOutSchema для сериализации данных
+        return await HouseOutSchema.from_tortoise_orm(house)
+
+    raise HTTPException(status_code=404, detail="Нет такого дома")
+
+async def add_review_to_house(id: UUID, review_data: str, rating: int) -> HouseOutSchema:
+    pass
--- services/backend/src/routes/houses.py
+++ services/backend/src/routes/houses.py
@@ -3,17 +3,31 @@ from typing import List
 import src.crud.houses as crud
 from src.schemas.houses import HouseOutSchema
 from src.main import logger
+from uuid import UUID
 
 router = APIRouter()
 
 @router.get("/houses/search", response_model=List[HouseOutSchema])
 async def search_houses(query: str):
-    logger.info(f'Тест')
     if not query:
-        return []
+        raise HTTPException(status_code=404, detail="Дом не найден")
     houses = await crud.get_house(query)
     logger.info(f'Дома {houses}')
-    if not houses:
-        return []
 
     return houses
+
+@router.get("/house/{id}", response_model=HouseOutSchema)
+async def get_house_by_id(id: UUID):
+    logger.info(f'Дома {id}')
+    house = await crud.get_house_by_id(id)
+    logger.info(f'Дома {house}')
+    if not house:
+        raise HTTPException(status_code=404, detail="Дом не найден")
+    return house
+
+@router.post("/house/{id}/reviews", response_model=HouseOutSchema)
+async def add_review_to_house(id: UUID, review_data: str, rating: int):
+    house = await crud.add_review_to_house(id, review_data, rating)
+    if not house:
+        raise HTTPException(status_code=404, detail="Дом не найден")
+    return house
